e3a3773f3d47fcec38e9fcf024c78394b16da518
Md Al-Amin <md.al.amin@portonics.com>
2025-11-10 00:32:54 +0600

    update test_CaseNo_1 File

--- src/tests/test_CaseNo_1.py
+++ src/tests/test_CaseNo_1.py
@@ -10,9 +10,10 @@ def test_home_page(page):
 
     home_page.visit_home_page()
     home_page.is_homePage_visible()
+    home_page.is_homePage_visible()
     home_page.click_product_button()
     product_page.check_is_user_navigate_to_all_products_page()
-    product_page.enter_product_name_and_search("Top")
+    product_page.enter_product_name_and_search("Blue Top")
     product_page.is_searched_product_visible("Top")
     product_page.all_the_search_related_product_are_visible()
     time.sleep(5)
